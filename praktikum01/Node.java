d13f9e71f5ef83671db061262186a8745f9d5d73
abhixyz1 <abhinayazrf@gmail.com>
2024-06-06 12:46:04 +0700

    Praktikum Binary Tree

---
+++ praktikum01/Node.java
@@ -0,0 +1,20 @@
+package praktikum01;
+
+/**
+ * @author Abhinaya
+ */
+
+public class Node{
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(){
+    }
+
+    public Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
